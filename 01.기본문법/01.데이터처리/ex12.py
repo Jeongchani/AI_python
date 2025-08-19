63fc80ddb1c6798e6f532e3f961245f2c0483b3d
helen-cho <hd9536@daum.net>
2025-08-19 10:24:04 +0900

    19

--- 01.기본문법/01.데이터처리/ex12.py
+++ 01.기본문법/01.데이터처리/ex12.py
@@ -1,12 +1,13 @@
 #list 타입
 names = ['홍길동', '심청이', '강감찬']
-print(names, type(names))
+names.pop(0)
+print(1, names, type(names))
 
 names.append('박명수')
 print(names, type(names))
 
 names.pop()
-print(names)
+print(3, names)
 
 names.insert(1, '박명수')
 names.append('박명수')
@@ -14,3 +15,6 @@ print(names)
 
 print(names.count('박명수'))
 print(names, names[1:3])
+print(5,names.remove('심청이'), names)
+names[0] = '김길동'
+print(6, names)
