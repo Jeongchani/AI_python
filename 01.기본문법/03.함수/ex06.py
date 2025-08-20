0334d580d7ade95a32a45ea9df3a15c9e9d39a6f
helen-cho <hd9536@daum.net>
2025-08-20 16:20:42 +0900

    20

--- 01.기본문법/03.함수/ex06.py
+++ 01.기본문법/03.함수/ex06.py
@@ -1,6 +1,22 @@
 from function import menuPrint, inputNum, grade
 
-scores = []
+scores = [
+    {'name':'이순신', 'kor':90, 'eng':88, 'mat':78},
+    {'name':'심청이', 'kor':95, 'eng':98, 'mat':98},
+]
+
+def search(name):
+    isFind = False
+    for index, s in enumerate(scores):
+        if s['name'].find(name) != -1:
+            isFind = True
+            tot = s['kor']+s['eng']+s['mat']
+            avg = tot/3
+            print(index, s['name'],s['kor'],s['eng'],s['mat'],f"{avg:.2f}",grade(avg))
+    if isFind==False:
+        print("검색 결과가 없습니다.")
+
+
 while True:
     menuPrint("성적관리")
     menu = input("메뉴선택>")
@@ -15,7 +31,27 @@ while True:
         scores.append({"name":name, "kor":kor, "eng":eng, "mat":mat})
         print("입력성공!")
     elif menu=="3": #목록
+        if len(scores)==0:
+            print("등록된 데이터가 없습니다!")
+            continue
         for s in scores:
             tot = s['kor']+s['eng']+s['mat']
             avg = tot/3
-            print(name, kor, eng, mat, f"{avg:.2f}", grade(avg))
+            print(s['name'],s['kor'],s['eng'],s['mat'],f"{avg:.2f}",grade(avg))
+    elif menu=="2": #검색
+        search_name = input("검색이름>")
+        search(search_name)
+
+    elif menu=="4": #삭제
+        del_name = input("삭제이름>")
+
+        isFind = False
+        for index, s in enumerate(scores):
+            if s['name'].find(del_name) != -1: #삭제할 이름을 찾은 경우
+                print(index, s['name'])
+                isFind = True
+        if isFind:
+            index = inputNum("삭제번호>")
+            scores.pop(index)
+        else:
+            print("삭제할 이름이 없습니다.")
