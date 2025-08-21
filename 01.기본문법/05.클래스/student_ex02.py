deb15883fd480eae2c0556a88983691d3c3ec2eb
helen-cho <hd9536@daum.net>
2025-08-21 17:03:01 +0900

    08-21

--- 01.기본문법/05.클래스/student_ex02.py
+++ 01.기본문법/05.클래스/student_ex02.py
@@ -1,10 +1,14 @@
 from student import Student
 
-students = []
+students = [
+    {'no':'01', 'name':'홍길동', 'dept':'전자과', 'birthday':'00-10-04'},
+    {'no':'02', 'name':'심청이', 'dept':'전기과', 'birthday':'02-12-17'},
+]
+
 while True:
-    print('----------------------')
-    print('|1.등록|2.목록|0.종료|')
-    print('----------------------')
+    print('------------------------------')
+    print('|1.등록|2.목록|3.삭제|0.종료|')
+    print('------------------------------')
     menu = input("메뉴선택>")
     if menu=="0":
         break
@@ -18,4 +22,13 @@ while True:
             stu = Student()
             stu.no = s['no']
             stu.name = s['name']
+            stu.dept = s['dept']
+            stu.birthday = s['birthday']
             stu.info_print()
+    elif menu=="3":
+        no = input("삭제번호>")
+        for idx, s in enumerate(students):
+            if no==s['no']:
+                students.pop(idx)
+                print('삭제완료!')
+                break
