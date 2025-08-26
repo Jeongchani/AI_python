954ada5925925620874fd5c1bcb7e4de458c32d4
helen-cho <hd9536@daum.net>
2025-08-26 11:42:21 +0900

    08-26

--- 01.기본문법/08.SQLite/insert.py
+++ 01.기본문법/08.SQLite/insert.py
@@ -8,6 +8,10 @@ cursor = con.cursor()
 
 sql="insert into juso(name, address) values('강감찬','부산시 동래구')"
 cursor.execute(sql)
+sql="insert into juso(name, address) values('홍길동','경기도 광명시')"
+cursor.execute(sql)
+sql="insert into juso(name, address) values('이순신','서울 강남구')"
+cursor.execute(sql)
 
 id = cursor.lastrowid
 
