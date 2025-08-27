b05782bbc00dfdf2d7526ceb6cd878e36a77412a
helen-cho <hd9536@daum.net>
2025-08-27 13:20:08 +0900

    08-27

--- 01.기본문법/08.SQLite/학사관리/database.py
+++ 01.기본문법/08.SQLite/학사관리/database.py
@@ -17,7 +17,7 @@ class Student(Dept):
         self.dept=0
 
     def print(self):
-        print(f'학번:{self.id}, 학과:{self.dname}({self.dept}), 이름:{self.name}')
+        print(f'학번:{self.id}, 이름:{self.name}, 학과:{self.dname}({self.dept})')
 
 def list():
     try:
@@ -37,13 +37,12 @@ def list():
     except Exception as err:
         print('목록 에러:', err)
     finally:
-        cur.close()
-        con.close()
+        pass
 
 def search(value):
     try:
         cur = con.cursor()
-        sql  ='select * from vstudent where name like ? or id like ? or dname like ?'
+        sql  ='1select * from vstudent where name like ? or id like ? or dname like ?'
         value = f'%{value}%'
         cur.execute(sql, (value, value, value,))
         rows = cur.fetchall()
@@ -61,9 +60,37 @@ def search(value):
     finally:
         pass
 
+def newID():
+    try:
+        cur = con.cursor()
+        sql ='select max(id)+1 from student'
+        cur.execute(sql)
+        row = cur.fetchone()
+        new_id = row[0]
+        return new_id
+    except Exception as err:
+        print('코드생성:', err)
+    finally:
+        pass
+
+def insert(stu):
+    try:
+        cur = con.cursor()
+        sql = 'insert into student(id, name, dept) values(?,?,?)'
+        cur.execute(sql, (stu.id, stu.name, stu.dept,))
+        con.commit()
+    except Exception as err:
+        print('입력오류:', err)
+    finally:
+        pass
+
 if __name__=='__main__':
-    value = input('검색어>')
-    students = search(value)
-    if students!=None:
-        for stu in students:
-            stu.print()
+    stu = Student()
+    stu.id = newID()
+    print(f'학번:{stu.id}')
+    stu.name=input('이름>')
+    if stu.name=='':
+        print('이름을  반드시 입력하세요!')
+    else:
+        stu.dept = int(input('학과>'))
+    insert(stu)
--- 01.기본문법/08.SQLite/학사관리/main.py
+++ 01.기본문법/08.SQLite/학사관리/main.py
@@ -8,18 +8,37 @@ while True:
         print('프로그램을 종료합니다.')
         break
     elif menu=='1':
-        pass
+        stu = Student()
+        stu.id = newID()
+        print(f'학번>{stu.id}')
+        while True:
+            stu.name = input('이름>')
+            if stu.name == '':
+                print('이름은 반드시 입력하세요!')
+                continue
+            else:
+                break
+        while True:
+            stu.dept = inputNum('학과>')
+            if stu.dept <1 or stu.dept >3:
+                print('학과코드는 1~3을 입력하세요!')
+                continue
+            else:
+                break
+        insert(stu)
+        print('학생등록성공!')
+
     elif menu=='2':
         while True:
             value = input('검색어>')
             if value=='':break
             students = search(value)
-            if len(students)==0:
+            if students==None:
                 print('검색결과가 없습니다.')
                 continue
             for stu in students:
                 stu.print()
-            print(f'{len(students)}명 학생이 존재합니다.') 
+            print(f'{len(students)}명 학생이 존재합니다.\n') 
     elif menu=='3':
         students = list()
         for stu in students:
