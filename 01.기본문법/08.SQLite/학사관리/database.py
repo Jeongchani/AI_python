fe3bd285bfb5b59ae1a41ec15e5550e0f6d393fe
helen-cho <hd9536@daum.net>
2025-08-27 11:18:34 +0900

    08-27

--- 01.기본문법/08.SQLite/학사관리/database.py
+++ 01.기본문법/08.SQLite/학사관리/database.py
@@ -20,21 +20,25 @@ class Student(Dept):
         print(f'학번:{self.id}, 학과:{self.dname}({self.dept}), 이름:{self.name}')
 
 def list():
-    cur = con.cursor()
-    sql = 'select s.*, d.name as dname from student as s, dept as d where s.dept=d.code'
-    cur.execute(sql)
-    rows = cur.fetchall()
-    list=[]
-    for row in rows:
-        stu = Student()
-        stu.id = row[0]
-        stu.dept = row[1]
-        stu.name = row[2]
-        stu.dname = row[3]
-        list.append(stu)
-    return list
+    try:
+        cur = con.cursor()
+        sql = 'select s.*, d.name as dname from student as s, dept as d where s.dept=d.code'
+        cur.execute(sql)
+        rows = cur.fetchall()
+        list=[]
+        for row in rows:
+            stu = Student()
+            stu.id = row[0]
+            stu.dept = row[1]
+            stu.name = row[2]
+            stu.dname = row[3]
+            list.append(stu)
+        return list
+    except Exception as err:
+        print('목록 에러:', err)
 
 if __name__=='__main__':
     students = list()
-    for stu in students:
-        stu.print()
+    if students!=None:
+        for stu in students:
+            stu.print()
