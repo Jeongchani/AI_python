a02c8bff6f4aa317218742c0e9a2729f6f170c65
helen-cho <hd9536@daum.net>
2025-09-05 17:22:15 +0900

    09-05

--- 02.데이터수집/05.API/ex03.py
+++ 02.데이터수집/05.API/ex03.py
@@ -16,28 +16,34 @@ def searchAddress(address):
             search_list.append(store)
     return search_list
 
-def createMap():
-    y = 37.5511225714939
-    x = 126.987867837993
-    location = (y, x)
-    
+def createMap(list, address):
+    y =list[0]['y']
+    x =list[0]['x']
+    location=(y, x)
     map = folium.Map(location, zoom_start=15, width='100%', height='100%')
-    popup=folium.Popup('남산타워')
-    folium.Marker(
-        location,
-        popup,
-        icon=folium.Icon(color='blue', icon='glyphicon-road')
-    ).add_to(map)
 
-    map.save('data/map/남산타워.html')
+    for store in list:
+        location=(store['y'], store['x'])
+        text = f'{store["name"]}<br>{store["phone"]}<br>{store["address"]}'
+        popup=folium.Popup(text, max_width=200)
+        folium.Marker(
+            location,
+            popup,
+            icon=folium.Icon(color='blue', icon='glyphicon-road')
+        ).add_to(map)
+
+    map.save(f'data/map/{address}.html')
 
 if __name__=='__main__':
-    createMap()
-    # os.system('cls')
-    # while True:        
-    #     print()
-    #     address = input("매장주소>")
-    #     if address=='':break
-    #     search_list=searchAddress(address)
-    #     if len(search_list)==0:
-    #         print('검색한 매장이 없습니다.')
+    os.system('cls')
+    while True:        
+        print()
+        address = input("매장주소>")
+        if address=='':break
+        search_list=searchAddress(address)
+        if len(search_list)==0:
+            print('검색한 매장이 없습니다.')
+        else:
+            sel=input('지도를 출력하실래요(Y)>')
+            if sel=='Y' or sel=='y':
+                createMap(search_list, address)
