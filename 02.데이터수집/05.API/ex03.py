d1238f6b0f87c65ce4401309116da7336c415b19
helen-cho <hd9536@daum.net>
2025-09-05 16:52:11 +0900

    09-05

--- 02.데이터수집/05.API/ex03.py
+++ 02.데이터수집/05.API/ex03.py
@@ -1,5 +1,6 @@
 import json
 import os
+import folium
 
 def getAddress():
     with open('data/hollys_location.json', 'r', encoding='utf8') as file:
@@ -15,10 +16,28 @@ def searchAddress(address):
             search_list.append(store)
     return search_list
 
+def createMap():
+    y = 37.5511225714939
+    x = 126.987867837993
+    location = (y, x)
+    
+    map = folium.Map(location, zoom_start=15, width='100%', height='100%')
+    popup=folium.Popup('남산타워')
+    folium.Marker(
+        location,
+        popup,
+        icon=folium.Icon(color='blue', icon='glyphicon-road')
+    ).add_to(map)
+
+    map.save('data/map/남산타워.html')
+
 if __name__=='__main__':
-    os.system('cls')
-    print()
-    address = input("매장주소>")
-    search_list=searchAddress(address)
-    if len(search_list)==0:
-        print('검색한 매장이 없습니다.')
+    createMap()
+    # os.system('cls')
+    # while True:        
+    #     print()
+    #     address = input("매장주소>")
+    #     if address=='':break
+    #     search_list=searchAddress(address)
+    #     if len(search_list)==0:
+    #         print('검색한 매장이 없습니다.')
