84785b9575add77fa8fcc8132766e7d23e8a2ef3
helen-cho <hd9536@daum.net>
2025-09-10 15:50:17 +0900

    09-10

---
+++ 03.데이터분석/프로젝트/ex05.py
@@ -0,0 +1,71 @@
+import os
+import pandas as pd
+
+file_score ='c:/python/03.데이터분석/data/학생성적.csv'
+file_info ='c:/python/03.데이터분석/data/학생정보.csv'
+
+def inputNum(title):
+    while True:
+        num = input(title)
+        if num=='':
+            return ''
+        elif not num.isnumeric():
+            print('점수는 숫자로입력하세요.')
+        else:
+            return int(num)
+        
+def submenu():
+    while True:
+        score = pd.read_csv(file_score, index_col='지원번호')
+        info = pd.read_csv(file_info, index_col='지원번호')
+        df = info.join(score)
+        df.fillna(0, inplace=True)
+        cols = ['국어', '영어', '수학', '사회', '과학']
+
+        os.system('cls')
+        print('-' * 50)
+        print('***************** 성  적  관  리 ***************')
+        print('-' * 50)
+        print('1.등록|2.목록|3.검색|4.삭제|5.수정|0.종료')
+        print('-' * 50)
+        menu = input('메뉴선택>')
+        if menu=='0':
+            break
+        elif menu=='1':
+            no = inputNum('지원번호>')
+            if not no in info.index:
+                print('등록된 지원번호가 없습니다.')
+            elif no in score.index:
+                print('이미 성적을 입력했습니다.')
+            else:
+                grade=[]
+                for col in cols:
+                    num = inputNum(f'{col}>')
+                    if num=='': num=0
+                    grade.append(num)
+                score.loc[no]=grade
+                score.to_csv(file_score)
+                print('등록완료!')
+            input('아무키나 누르세요!')
+        elif menu=='2': #목록
+            for idx in df.index:
+                row = df.loc[idx]
+                print(f'지원번호:{idx}', end=' ')
+                print(f'이름:{row["이름"]}', end=' ')
+                for col in cols:
+                    print(f'{col}:{row[col]:.0f}', end=' ')
+                print()
+                print('-' * 60)
+
+            input('아무키나 누르세요!')
+        elif menu=='3':
+            input('아무키나 누르세요!')
+        elif menu=='4':
+            input('아무키나 누르세요!')
+        elif menu=='5':
+            input('아무키나 누르세요!')
+        else:
+            input('0~5번 숫자를 입력하세요!')
+
+if __name__=='__main__':
+    submenu()
